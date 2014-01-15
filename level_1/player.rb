47d7d591cd664d700cd12f23bbeff0e072d88ed7
jcrey <jc_rip@hotmail.com>
2014-01-14 23:50:15 -0600

    Added level 1

---
+++ level_1/player.rb
@@ -0,0 +1,15 @@
+class Player
+
+	def initialize
+		@directionOfStairs = nil
+	end
+
+  def play_turn(warrior)
+    
+    @directionOfStairs = warrior.direction_of_stairs
+
+    warrior.walk!(@directionOfStairs)
+
+
+  end
+end
